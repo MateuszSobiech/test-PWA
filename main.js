db9a6e7af596b119013b2ee44493408c057fd204
Mateusz Sobiech <lordpython8@gmail.com>
2021-11-09 13:22:02 +0100

    init PWA

---
+++ main.js
@@ -0,0 +1,16 @@
+// let deferredPrompt;
+
+// window.addEventListener('beforeinstallprompt', (e) => {
+//   // Prevent Chrome 67 and earlier from automatically showing the prompt
+//   e.preventDefault();
+//   // Stash the event so it can be triggered later.
+//   deferredPrompt = e;
+//   // Update UI notify the user they can add to home screen
+// });
+
+// deferredPrompt.prompt();
+
+// // Check if the app is succesfully installed
+// window.addEventListener('appinstalled', (evt) => {
+//   app.logEvent('a2hs', 'installed');
+// });
