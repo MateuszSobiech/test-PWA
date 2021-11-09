6d4dd5b7e1a5a5354cb973906f0c8b101dcb8b14
Mateusz Sobiech <lordpython8@gmail.com>
2021-11-09 13:27:56 +0100

    add js

--- main.js
+++ main.js
@@ -1,16 +1,8 @@
-// let deferredPrompt;
+let deferredPrompt;
 
-// window.addEventListener('beforeinstallprompt', (e) => {
-//   // Prevent Chrome 67 and earlier from automatically showing the prompt
-//   e.preventDefault();
-//   // Stash the event so it can be triggered later.
-//   deferredPrompt = e;
-//   // Update UI notify the user they can add to home screen
-// });
-
-// deferredPrompt.prompt();
-
-// // Check if the app is succesfully installed
-// window.addEventListener('appinstalled', (evt) => {
-//   app.logEvent('a2hs', 'installed');
-// });
+window.addEventListener('beforeinstallprompt', (e) => {
+  // Prevent Chrome 67 and earlier from automatically showing the prompt
+  e.preventDefault();
+  // Stash the event so it can be triggered later.
+  deferredPrompt = e;
+});
